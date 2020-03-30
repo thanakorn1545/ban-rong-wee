c59bdb597d8e8a5704512b7a66e2a7db439ad5ed
thanakorn1545 <62874191+thanakorn1545@users.noreply.github.com>
2020-03-30 16:24:24 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php 
-  $access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+  $access_token = 'bD1buYHvPZZ1RkFAPO8iggutYfg6UdqkZby1O0dZDQmfbnj6MvxTBOXyXsWvekmuDksiFfPbFao/6yjJJ71Jsq//jIuVBymk66FAkY1Vvpu2ENjyHaxz1OsgrATyyqGTxTOT/aEBeYaKomVwiYXTNgdB04t89/1O/w1cDnyilFU=';
   $url = 'https://api.line.me/v1/oauth/verify';
   $headers = array('Authorization: Bearer ' . bD1buYHvPZZ1RkFAPO8iggutYfg6UdqkZby1O0dZDQmfbnj6MvxTBOXyXsWvekmuDksiFfPbFao/6yjJJ71Jsq//jIuVBymk66FAkY1Vvpu2ENjyHaxz1OsgrATyyqGTxTOT/aEBeYaKomVwiYXTNgdB04t89/1O/w1cDnyilFU=);
   $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
